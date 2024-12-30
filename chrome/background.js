df077d465fb2bf6d3ca804bdefacc2012b58a395
chrwoizi <christian@woizischke.de>
2024-12-30 17:44:10 +0100

    migrate to manifest v3

--- chrome/background.js
+++ chrome/background.js
@@ -1,91 +1,83 @@
-const woiziconsent = function (obj) {
-    if (obj instanceof woiziconsent) return obj
-    if (!(this instanceof woiziconsent)) return new woiziconsent(obj)
-}
-window.woiziconsent = woiziconsent;
-
 async function executeScripts(scripts) {
-    const file = scripts.files.shift();
-    const details = { file: file, runAt: "document_idle", allFrames: true }
+  const file = scripts.files.shift();
 
-    try {
-        await chrome.tabs.executeScript(scripts.tabId, details)
-    }
-    catch (e) {
-        logger.error('executeScripts error:', e.message)
-    }
+  try {
+    await chrome.scripting.executeScript({
+      target: { tabId: scripts.tabId, allFrames: true },
+      files: [file],
+    });
+  } catch (e) {
+    console.error("executeScript error for tab " + scripts.tabId, e.message);
+  }
 
-    if (scripts.files.length > 0) {
-        await executeScripts(scripts);
-    }
+  if (scripts.files.length > 0) {
+    await executeScripts(scripts);
+  }
 }
 
 chrome.tabs.onUpdated.addListener(async (_tabId, changeInfo, tab) => {
-    if (tab.url.indexOf('http://') === -1 && tab.url.indexOf('https://') === -1) {
-        console.log('not running on url ' + tab.url);
-        return;
+  if (tab.url.indexOf("http://") === -1 && tab.url.indexOf("https://") === -1) {
+    console.log("not running on url " + tab.url);
+    return;
+  }
+  if (changeInfo.status === "complete") {
+    const message = {
+      tabId: tab.id,
+      version: chrome.runtime.getManifest().version,
+      logLevel: 4,
+    };
+
+    let pong;
+    try {
+      console.log("ping page");
+      pong = await chrome.tabs.sendMessage(tab.id, {
+        ...message,
+        ping: true,
+      });
+    } catch (e) {
+      console.log(e.toString());
     }
-    if (changeInfo.status === 'complete') {
-        const message = {
-            tabId: tab.id,
-            version: chrome.runtime.getManifest().version,
-            logLevel: logger.level
-        }
 
-        let pong;
-        try {
-            logger.log('ping page');
-            pong = await chrome.tabs.sendMessage(tab.id, {
-                ...message,
-                ping: true,
-            })
-        }
-        catch (e) {
-            logger.log(e.toString());
-        }
-
-        if (!pong || !pong.status == "pong") {
-            logger.log('injecting scripts into page');
-            await executeScripts(
-                {
-                    tabId: tab.id,
-                    files: ['logger.js', 'inuserview.js', 'common.js', 'page.js']
-                }
-            )
-        }
-
-        const attempts = [1, 100, 200, 500, 500, 1000, 1000];
-        let attempt = 0;
-
-        function runAttempt() {
-            logger.log('attempt ' + (attempt + 1) + '/' + attempts.length);
-            const startTime = new Date();
-            chrome.tabs.sendMessage(tab.id, message).then(result => {
-                const duration = new Date().getTime() - startTime.getTime();
-                logger.log(result, `${(duration / 1000).toFixed(2)} seconds`);
-                if (result.done) {
-                    logger.log('done');
-                }
-                else {
-                    if (!result.success) {
-                        attempt++;
-                        if (attempt < attempts.length) {
-                            const timeout = attempts[attempt];
-                            setTimeout(runAttempt, timeout);
-                        }
-                        else {
-                            chrome.tabs.sendMessage(tab.id, {
-                                ...message,
-                                fatal: true,
-                            });
-                        }
-                    }
-                }
-            })
-                .catch(e => {
-                    logger.log(e.toString())
-                })
-        }
-        setTimeout(runAttempt, attempts[attempt]);
+    if (!pong || pong.status !== "pong") {
+      console.log("injecting scripts into page");
+      await executeScripts({
+        tabId: tab.id,
+        files: ["logger.js", "inuserview.js", "common.js", "page.js"],
+      });
     }
+
+    const attempts = [1, 100, 200, 500, 500, 1000, 1000];
+    let attempt = 0;
+
+    function runAttempt() {
+      console.log("attempt " + (attempt + 1) + "/" + attempts.length);
+      const startTime = new Date();
+      chrome.tabs
+        .sendMessage(tab.id, message)
+        .then((result) => {
+          const duration = new Date().getTime() - startTime.getTime();
+          console.log(result, `${(duration / 1000).toFixed(2)} seconds`);
+          if (result.done) {
+            console.log("done");
+          } else {
+            if (!result.success) {
+              attempt++;
+              if (attempt < attempts.length) {
+                const timeout = attempts[attempt];
+                setTimeout(runAttempt, timeout);
+              } else {
+                chrome.tabs.sendMessage(tab.id, {
+                  ...message,
+                  fatal: true,
+                });
+              }
+            }
+          }
+        })
+        .catch((e) => {
+          console.log(e.toString());
+        });
+    }
+    setTimeout(runAttempt, attempts[attempt]);
+  }
 });
--- chrome/page.js
+++ chrome/page.js
@@ -1,216 +1,214 @@
-'use strict';
+"use strict";
 
 function getLogoutButtons(visibleElements) {
-    const logoutPatterns = [
-        // english
-        /.{0,30}\blog-?\s*-?out\b.{0,30}/i,
-        /.{0,30}\bsign-?\s*-?out\b.{0,30}/i,
+  const logoutPatterns = [
+    // english
+    /.{0,30}\blog-?\s*-?out\b.{0,30}/i,
+    /.{0,30}\bsign-?\s*-?out\b.{0,30}/i,
 
-        // german
-        /.{0,30}\bausloggen\b.{0,30}/i,
-        /.{0,30}\babmelden\b.{0,30}/i,
-    ];
+    // german
+    /.{0,30}\bausloggen\b.{0,30}/i,
+    /.{0,30}\babmelden\b.{0,30}/i,
+  ];
 
-    const buttons = getButtons(visibleElements);
-    let logoutButtons = buttons.filter(x => elementTextMatches(x, logoutPatterns));
+  const buttons = getButtons(visibleElements);
+  let logoutButtons = buttons.filter((x) =>
+    elementTextMatches(x, logoutPatterns)
+  );
 
-    return logoutButtons;
+  return logoutButtons;
 }
 
 async function isLoggedIn() {
-    const logoutButtons = getLogoutButtons();
-    if (logoutButtons.length > 0) {
-        logger.log('logout button found');
-        logger.log(logoutButtons);
-        return true;
-    }
+  const logoutButtons = getLogoutButtons();
+  if (logoutButtons.length > 0) {
+    logger.log("logout button found");
+    logger.log(logoutButtons);
+    return true;
+  }
 
-    try {
-        if (readStorage(localStorage).find(x => isJWT(x.value))) {
-            logger.log('jwt found in localStorage');
-            return true;
-        }
+  try {
+    if (readStorage(localStorage).find((x) => isJWT(x.value))) {
+      logger.log("jwt found in localStorage");
+      return true;
     }
-    catch (e) {
-        logger.log('could not load localStorage');
-        logger.log(e);
-    }
+  } catch (e) {
+    logger.log("could not load localStorage");
+    logger.log(e);
+  }
 
-    try {
-        if (readStorage(sessionStorage).find(x => isJWT(x.value))) {
-            logger.log('jwt found in sessionStorage');
-            return true;
-        }
+  try {
+    if (readStorage(sessionStorage).find((x) => isJWT(x.value))) {
+      logger.log("jwt found in sessionStorage");
+      return true;
     }
-    catch (e) {
-        logger.log('could not load sessionStorage');
-        logger.log(e);
-    }
+  } catch (e) {
+    logger.log("could not load sessionStorage");
+    logger.log(e);
+  }
 
-    try {
-        const cookies = await readCookies();
-        if (cookies.find(x => isJWT(x.value))) {
-            logger.log('jwt found in cookies');
-            return true;
-        }
+  try {
+    const cookies = await readCookies();
+    if (cookies.find((x) => isJWT(x.value))) {
+      logger.log("jwt found in cookies");
+      return true;
     }
-    catch (e) {
-        logger.log('could not load cookies');
-        logger.log(e);
-    }
+  } catch (e) {
+    logger.log("could not load cookies");
+    logger.log(e);
+  }
 
-    return false;
+  return false;
 }
 
 function isPositionInFlowDirection(element, refElement) {
-    const elementRect = element.getBoundingClientRect();
-    const elementMiddleX = elementRect.left + elementRect.width / 2 + window.scrollX;
-    const elementMiddleY = elementRect.top + elementRect.height / 2 + window.scrollY;
+  const elementRect = element.getBoundingClientRect();
+  const elementMiddleX =
+    elementRect.left + elementRect.width / 2 + window.scrollX;
+  const elementMiddleY =
+    elementRect.top + elementRect.height / 2 + window.scrollY;
 
-    const refElementRect = refElement.getBoundingClientRect();
+  const refElementRect = refElement.getBoundingClientRect();
 
-    return elementMiddleX > refElementRect.left && elementMiddleY > refElementRect.top;
+  return (
+    elementMiddleX > refElementRect.left && elementMiddleY > refElementRect.top
+  );
 }
 
 async function getCookieConsentButtons(visibleElements) {
-    const cookiesPattern = [
-        /\bcookies\b/i,
-        /\bdatenschutz/i
-    ];
+  const cookiesPattern = [/\bcookies\b/i, /\bdatenschutz/i];
 
-    const acceptPatterns = [
-        // english
-        /.{0,30}\baccept\b.{0,30}/i,
-        /.{0,30}\bsave\b.{0,30}/i,
-        /.{0,30}\bgot\s+it\b.{0,30}/i,
-        /.{0,30}\bi\s+understand\b.{0,30}/i,
-        /.{0,30}\bunderstood\b.{0,30}/i,
-        /.{0,30}\ballow\b.{0,30}/i,
-        /.{0,30}\bagree\b.{0,30}/i,
-        /.{0,30}\backnowledged?\b.{0,30}/i,
+  const acceptPatterns = [
+    // english
+    /.{0,30}\baccept\b.{0,30}/i,
+    /.{0,30}\bsave\b.{0,30}/i,
+    /.{0,30}\bgot\s+it\b.{0,30}/i,
+    /.{0,30}\bi\s+understand\b.{0,30}/i,
+    /.{0,30}\bunderstood\b.{0,30}/i,
+    /.{0,30}\ballow\b.{0,30}/i,
+    /.{0,30}\bagree\b.{0,30}/i,
+    /.{0,30}\backnowledged?\b.{0,30}/i,
 
-        // german
-        /.{0,30}\bakzeptieren?\b.{0,30}/i,
-        /.{0,30}\bannehmen\b.{0,30}/i,
-        /.{0,30}\bzustimmen\b.{0,30}/i,
-        /.{0,30}\bstimme\s+zu\b.{0,30}/i,
-        /.{0,30}\b(einstellungen\s+)?speichern\b.{0,30}/i,
-        /.{0,30}\bin\s+ordnung\b.{0,30}/i,
-        /.{0,30}\beinverstanden\b.{0,30}/i,
-        /.{0,30}\bverstanden\b.{0,30}/i,
-        /.{0,30}\bstimme\s+(dem\s+)?zu\b.{0,30}/i,
-        /.{0,30}\bbestätigen?\b.{0,30}/i,
-        /.{0,30}\bokay\b.{0,30}/i,
-        /.{0,30}\bzulassen\b.{0,30}/i,
-        /.{0,30}\blasse\s+zu\b.{0,30}/i,
-        /.{0,30}\berlauben?\b.{0,30}/i,
-        /.{0,30}\beinwilligen\b.{0,30}/i,
-        /.{0,30}\bich\s+willige\s+ein\b.{0,30}/i,
-        /.{0,30}\bweiter\s+zur\s+seite\b.{0,30}/i,
-        /.{0,30}\b(alle\s+)?cookies\s+akzeptieren\b.{0,30}/i,
-    ];
+    // german
+    /.{0,30}\bakzeptieren?\b.{0,30}/i,
+    /.{0,30}\bannehmen\b.{0,30}/i,
+    /.{0,30}\bzustimmen\b.{0,30}/i,
+    /.{0,30}\bstimme\s+zu\b.{0,30}/i,
+    /.{0,30}\b(einstellungen\s+)?speichern\b.{0,30}/i,
+    /.{0,30}\bin\s+ordnung\b.{0,30}/i,
+    /.{0,30}\beinverstanden\b.{0,30}/i,
+    /.{0,30}\bverstanden\b.{0,30}/i,
+    /.{0,30}\bstimme\s+(dem\s+)?zu\b.{0,30}/i,
+    /.{0,30}\bbestätigen?\b.{0,30}/i,
+    /.{0,30}\bokay\b.{0,30}/i,
+    /.{0,30}\bzulassen\b.{0,30}/i,
+    /.{0,30}\blasse\s+zu\b.{0,30}/i,
+    /.{0,30}\berlauben?\b.{0,30}/i,
+    /.{0,30}\beinwilligen\b.{0,30}/i,
+    /.{0,30}\bich\s+willige\s+ein\b.{0,30}/i,
+    /.{0,30}\bweiter\s+zur\s+seite\b.{0,30}/i,
+    /.{0,30}\b(alle\s+)?cookies\s+akzeptieren\b.{0,30}/i,
+  ];
 
-    const minTextLength = 50;
-    const maxTreeDistance = 14;
-    const maxPixelDistance = 400;
+  const minTextLength = 50;
+  const maxTreeDistance = 14;
+  const maxPixelDistance = 400;
 
-    const elements = visibleElements || getVisibleElements();
-    const textElements = elements.filter((x) => {
-        return (
-            x.innerText?.length > minTextLength &&
-            matchesPatterns(x.innerText, cookiesPattern)
-        );
-    });
-
-    if (textElements.length === 0) return [];
-
-    const buttons = getButtons(elements);
-    const acceptButtons = buttons.filter((x) =>
-        elementTextMatches(x, acceptPatterns),
+  const elements = visibleElements || getVisibleElements();
+  const textElements = elements.filter((x) => {
+    return (
+      x.innerText?.length > minTextLength &&
+      matchesPatterns(x.innerText, cookiesPattern)
     );
+  });
 
-    const cookieAcceptButtons = acceptButtons.filter((x) => {
-        const treeDistance = minTreeDistance(x, textElements);
-        if (treeDistance > maxTreeDistance) {
-            return false;
-        }
+  if (textElements.length === 0) return [];
 
-        const pixelDistance = minEdgePixelDistance(x, textElements);
-        if (pixelDistance > maxPixelDistance) {
-            return false;
-        }
+  const buttons = getButtons(elements);
+  const acceptButtons = buttons.filter((x) =>
+    elementTextMatches(x, acceptPatterns)
+  );
 
-        const isInFlowOfTextElement = textElements.find((y) =>
-            isPositionInFlowDirection(x, y),
-        );
-        if (!isInFlowOfTextElement) {
-            return false;
-        }
+  const cookieAcceptButtons = acceptButtons.filter((x) => {
+    const treeDistance = minTreeDistance(x, textElements);
+    if (treeDistance > maxTreeDistance) {
+      return false;
+    }
 
-        return true;
-    });
+    const pixelDistance = minEdgePixelDistance(x, textElements);
+    if (pixelDistance > maxPixelDistance) {
+      return false;
+    }
 
-    return sortByMinTreeDistance(cookieAcceptButtons, textElements);
+    const isInFlowOfTextElement = textElements.find((y) =>
+      isPositionInFlowDirection(x, y)
+    );
+    if (!isInFlowOfTextElement) {
+      return false;
+    }
+
+    return true;
+  });
+
+  return sortByMinTreeDistance(cookieAcceptButtons, textElements);
 }
 
 chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
-    woiziconsentContent(request).then(sendResponse);
-    return true;
-})
+  woiziconsentContent(request).then(sendResponse);
+  return true;
+});
 
 async function woiziconsentContent(request) {
-    try {
-        if (request.ping) {
-            return { status: "pong" }
-        }
+  try {
+    if (request.ping) {
+      return { status: "pong" };
+    }
 
-        if (!window.logger || !getButtons) {
-            return { success: false };
-        }
+    if (!window.logger || !getButtons) {
+      return { success: false };
+    }
 
-        logger.level = request.logLevel || logger.DEBUG
+    logger.level = request.logLevel || logger.DEBUG;
 
-        if (window.top !== window.self) {
-            if (!inuserview(document.body)) {
-                return { success: false };
-            }
-        }
-
-        var storageKey = 'woiziconsent';
-
-        if (localStorage.getItem(storageKey) === request.version) {
-            logger.log('failed in the past');
-            return { done: true };
-        }
-
-        if (request.fatal) {
-            localStorage.setItem(storageKey, request.version);
-        }
-
-        const elements = getVisibleElements()
-
-        if (await isLoggedIn(elements)) {
-            logger.log('is logged in');
-            return { done: true };
-        }
-
-        const buttons = await getCookieConsentButtons(elements);
-        if (buttons.length > 0) {
-            logger.log('automatically accepting cookie consent');
-            buttons.forEach(x => x.click());
-            localStorage.setItem(storageKey, request.version);
-            return { success: true };
-        }
-
+    if (window.top !== window.self) {
+      if (!inuserview(document.body)) {
         return { success: false };
+      }
     }
-    catch (e) {
-        try {
-            if (logger) logger.log(e);
-        }
-        catch (e2) {
-            console.log(e);
-        }
-        throw e;
+
+    var storageKey = "woiziconsent";
+
+    if (localStorage.getItem(storageKey) === request.version) {
+      logger.log("failed in the past");
+      return { done: true };
     }
+
+    if (request.fatal) {
+      localStorage.setItem(storageKey, request.version);
+    }
+
+    const elements = getVisibleElements();
+
+    if (await isLoggedIn(elements)) {
+      logger.log("is logged in");
+      return { done: true };
+    }
+
+    const buttons = await getCookieConsentButtons(elements);
+    if (buttons.length > 0) {
+      logger.log("automatically accepting cookie consent");
+      buttons.forEach((x) => x.click());
+      localStorage.setItem(storageKey, request.version);
+      return { success: true };
+    }
+
+    return { success: false };
+  } catch (e) {
+    try {
+      if (logger) logger.log(e);
+    } catch (e2) {
+      console.log(e);
+    }
+    throw e;
+  }
 }
